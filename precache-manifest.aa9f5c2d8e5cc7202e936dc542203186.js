d64ae8da8188651abc6474cec3bfd8d6d68ec4de
fredcoder <freddy.clavijo@gmail.com>
2020-09-10 14:23:02 +1000

    Updates

---
+++ precache-manifest.aa9f5c2d8e5cc7202e936dc542203186.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14455c7a091e150009fb1ecb52fe7371",
+    "url": "/index.html"
+  },
+  {
+    "revision": "328c7d4ea9c6f55c481b",
+    "url": "/static/css/main.c000ced7.chunk.css"
+  },
+  {
+    "revision": "36a0a88bddc73532a6e4",
+    "url": "/static/js/2.5d055729.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.5d055729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328c7d4ea9c6f55c481b",
+    "url": "/static/js/main.cc97ef23.chunk.js"
+  },
+  {
+    "revision": "05a9d70d3f8c91461855",
+    "url": "/static/js/runtime-main.0f14e458.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "409c7f79a42e56c785f50ed37535f0be",
+    "url": "/static/media/Montserrat-Light.409c7f79.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "a0602257b32df73be81fe2dcbf33473f",
+    "url": "/static/media/WebSymbols-Regular.a0602257.otf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
