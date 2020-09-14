3246b6b55dd5e01403a20fb96ce66d56f7aa5e7b
fredcoder <freddy.clavijo@gmail.com>
2020-09-14 19:01:12 +1000

    Add Contact Form + Social links

--- src/components/AboutMe/page.jsx
+++ src/components/AboutMe/page.jsx
@@ -5,6 +5,9 @@ import './styles.css';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faJs, faHtml5, faCss3Alt, faReact } from '@fortawesome/free-brands-svg-icons'
+
 class Page extends Component {
 
     state = {
@@ -20,13 +23,23 @@ class Page extends Component {
         return <React.Fragment>
             <section id="aboutMe" data-aos="fade-up">
                 <section id="wrapper">
-                    <div id="aboutMe-mark"></div>
-                    <img src={photo} alt="photo" data-aos="fade-up"/>
+                    <img src={photo} alt="photo" data-aos="fade-up" />
                     <h1 id="title" data-aos="fade-up">
                         About Me
                     </h1>
                     <p id="description" data-aos="fade-up">
                         I am a Full Stack Developer and system administrator with over 6+ years of experience in software engineering and developing new features and apps for different products and companies by using the best programming practices such as design patterns and readable documentation.
+                    </p>
+                    <h2 id="title" data-aos="fade-up">
+                        Skills
+                    </h2>
+                    <p id="skills" data-aos="fade-up">
+                        <span>
+                            <FontAwesomeIcon icon={faJs} />
+                            <FontAwesomeIcon icon={faHtml5} />
+                            <FontAwesomeIcon icon={faCss3Alt} />
+                            <FontAwesomeIcon icon={faReact} />
+                        </span>
                     </p>
                 </section>
             </section>
--- src/components/Contact/page.jsx
+++ src/components/Contact/page.jsx
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import photo from '../../assets/images/photo.jpg';
 import './styles.css';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
 class Page extends Component {
 
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.submitForm = this.submitForm.bind(this);
+        this.state = {
+            status: ""
+        };
     }
 
     componentDidMount() {
         AOS.init();
     }
 
+    submitForm(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                form.reset();
+                this.setState({ status: "SUCCESS" });
+            } else {
+                this.setState({ status: "ERROR" });
+            }
+        };
+        xhr.send(data);
+    }
+
     render() {
+
+        const { status } = this.state;
 
         return <React.Fragment>
             <section id="contact" data-aos="fade-up">
@@ -23,8 +49,24 @@ class Page extends Component {
                     <h1 id="title">
                         Contact
                     </h1>
-                    <p id="description">
-                        I am a Full Stack Developer and system administrator with over 6+ years of experience in software engineering and developing new features and apps for different products and companies by using the best programming practices such as design patterns and readable documentation.
+                    <form
+                      onSubmit={this.submitForm}
+                      action="https://formspree.io/meqpjajk"
+                      method="POST"
+                      id="contact-form" 
+                      data-aos="fade-up">
+                        <input type="text" name="name" placeholder="Your Name"/>
+                        <input type="email" name="_replyto" placeholder="Your Email"/>
+                        <textarea name="message"  placeholder="Message"></textarea>
+                        <input type="submit" value="Send"/>
+                    </form>
+                    <hr  data-aos="fade-up"/>
+                    <p id="description" data-aos="fade-up">
+                        <span>
+                            <a href="https://www.linkedin.com/in/freddy-ernesto-clavijo-fernandez-3b144453/"><FontAwesomeIcon icon={faLinkedin} /></a>
+                            <a href="https://www.instagram.com/fredcoder/"><FontAwesomeIcon icon={faInstagram} /></a>
+                            <a href="https://github.com/fredcoder"><FontAwesomeIcon icon={faGithub} /></a>
+                        </span>
                     </p>
                 </section>
             </section>
