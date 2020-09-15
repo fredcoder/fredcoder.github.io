d03bdf086cf99e055f0eb4762cf02d87e9b12bef
fredcoder <freddy.clavijo@gmail.com>
2020-09-15 21:19:39 +1000

    Update Projects Component

--- src/components/Projects/page.jsx
+++ src/components/Projects/page.jsx
@@ -6,7 +6,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faJs, faHtml5, faCss3Alt, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faJs, faHtml5, faCss3Alt, faReact } from '@fortawesome/free-brands-svg-icons';
 
 
 class Page extends Component {
@@ -27,19 +27,19 @@ class Page extends Component {
                     <h1 id="title" data-aos="fade-up">
                         Projects
                     </h1>
+                    <h2>
+                        Weather App
+                    </h2>
                     <div className="project-top" data-aos="fade-right">
                         <img src={weatherApp} alt="photo" />
                     </div>
-                    <div className="project-bottom">
-                        <h2 data-aos="fade-left">
-                            Weather App
-                        </h2>
-                        <p className="description" data-aos="fade-left">
-                            A very useful React application that gives users the current status and the daily and hourly forecast
+                    <div className="project-bottom" data-aos="fade-left">
+                        <p className="description">
+                            A very useful React application that gives users the current status of the daily and hourly forecast
                             of the weather in a graphical and very detailed way from their geolocation with real-time data obtained
                             from OpenWeather global services.
                         </p>
-                        <p className="technologies" data-aos="fade-left">
+                        <p className="technologies">
                             Developed with
                             <span>
                                 <FontAwesomeIcon icon={faJs} />
@@ -48,11 +48,11 @@ class Page extends Component {
                                 <FontAwesomeIcon icon={faReact} />
                             </span>
                         </p>
-                        <a data-aos="fade-left" className="link" href="https://fredcoder.github.io/weather/">See Live</a>
+                        <a className="link" href="https://fredcoder.github.io/weather/">See Live</a>
                     </div>
-                    <div className="clearfix"></div>
                 </section>
             </section>
+            <div className="clearfix"></div>
         </React.Fragment>
     }
 }
