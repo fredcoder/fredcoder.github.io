f222aee38c192bb133f364e954b8d9091a0d56ef
fredcoder <freddy.clavijo@gmail.com>
2020-09-15 21:27:50 +1000

    Update App styling

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Home from './components/Home';
 import AboutMe from './components/AboutMe';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <React.Fragment>
-      <Header />
-      <Home />
-      <AboutMe />
-      <Projects />
-      <Contact />
+        <Header />
+        <Home />
+        <AboutMe />
+        <Projects />
+        <Contact />
+        <Footer />
     </React.Fragment>
   );
 }
