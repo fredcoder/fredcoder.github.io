876aa73ef482ebaec85a6c08fb34f9b02438ce49
fredcoder <freddy.clavijo@gmail.com>
2020-09-20 23:32:20 +1000

    Update Global Styling

--- src/components/Projects/page.jsx
+++ src/components/Projects/page.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import weatherApp from '../../assets/images/weatherApp.jpg';
+import weatherApp from '../../assets/images/weatherApp.png';
 import './styles.css';
 
 import AOS from 'aos';
@@ -27,29 +27,31 @@ class Page extends Component {
                     <h1 id="title" data-aos="fade-up">
                         Projects
                     </h1>
-                    <hr/>
+                    <hr />
                     <h2>
                         Weather App
                     </h2>
-                    <div className="project-top" data-aos="fade-right">
-                        <img src={weatherApp} alt="photo" />
-                    </div>
-                    <div className="project-bottom" data-aos="fade-left">
-                        <p className="description">
-                        A very useful React application that provides users with the current status of 
-                        the daily and hourly weather forecast in a graphical and highly detailed way.  
-                        Through the geolocation api, real-time data is obtained from OpenWeather's global services.
+                    <div id="flex-box">
+                        <div className="project-top" data-aos="fade-right">
+                            <img src={weatherApp} alt="photo" />
+                        </div>
+                        <div className="project-bottom" data-aos="fade-left">
+                            <p className="description">
+                                A very useful React application that provides users with the current status of
+                                the daily and hourly weather forecast in a graphical and highly detailed way.
+                                Through the geolocation api, real-time data is obtained from OpenWeather's global services.
                         </p>
-                        <p className="technologies">
-                            Developed with
+                            <p className="technologies">
+                                Developed with
                             <span>
-                                <FontAwesomeIcon icon={faJs} />
-                                <FontAwesomeIcon icon={faHtml5} />
-                                <FontAwesomeIcon icon={faCss3Alt} />
-                                <FontAwesomeIcon icon={faReact} />
-                            </span>
-                        </p>
-                        <a className="link" href="https://fredcoder.github.io/weather/">See Live</a>
+                                    <FontAwesomeIcon icon={faJs} />
+                                    <FontAwesomeIcon icon={faHtml5} />
+                                    <FontAwesomeIcon icon={faCss3Alt} />
+                                    <FontAwesomeIcon icon={faReact} />
+                                </span>
+                            </p>
+                            <a className="link" href="https://fredcoder.github.io/weather/">See Live</a>
+                        </div>
                     </div>
                 </section>
             </section>
